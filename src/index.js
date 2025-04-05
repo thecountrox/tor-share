48ee2879a2e2a5d36d385a12ecca83f885616f43
Prajol David <prajoldavid2@gmail.com>
2025-04-05 21:28:28 +0530

    what even

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
-      contextIsolation: true,
+      contextIsolation: false,
     },
   });
 
